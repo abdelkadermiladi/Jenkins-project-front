0d78f0f03f3001344406f6f60862bba4fe85499e
Abdelkader Miladi <96490190+abdelkadermiladi@users.noreply.github.com>
2023-07-20 17:44:55 +0200

    update7

--- angular-project/src/app/time-range-picker/time-range-picker.component.ts
+++ angular-project/src/app/time-range-picker/time-range-picker.component.ts
@@ -6,17 +6,36 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './time-range-picker.component.html',
   styleUrls: ['./time-range-picker.component.css']
 })
-export class TimeRangePickerComponent {
+export class TimeRangePickerComponent implements OnInit {
+  nodeNames!: string[];
+  selectedNode!: string;
   startTime!: string;
   endTime!: string;
-  //jobBuilds table containing Job Name,	Build Number,	Date,	Duration
   jobBuilds: any[] = [];
-  //builds table containing Build Number, Duration : for the chart
-  builds: {jobname: string, buildnumber: string, duration: string }[] = [];
+  builds: { jobname: string, buildnumber: string, duration: string }[] = [];
   DisplayTable: boolean = false;
 
   constructor(private http: HttpClient) { }
 
+  ngOnInit() {
+    this.getNodes();
+  }
+
+  getNodes() {
+    this.http.get<any[]>('http://localhost:8082/api/nodeNames').subscribe(
+      (response: string[]) => {
+        this.nodeNames = response;
+      },
+      (error) => {
+        console.error('Error retrieving node names:', error);
+      }
+    );
+  }
+
+  // onNodeSelectionChange() {
+  //   // Handle the selected node change here
+  //   console.log('Selected node:', this.selectedNode);
+  // }
 
   submitForm() {
     const startDate = new Date(this.startTime);
@@ -25,27 +44,24 @@ export class TimeRangePickerComponent {
     const formattedStartDate = startDate.toISOString().slice(0, 16);
     const formattedEndDate = endDate.toISOString().slice(0, 16);
 
-    //the inputted startTime date and the endTime date converted to string 
     const dateData = {
       startTime: formattedStartDate,
-      endTime: formattedEndDate
+      endTime: formattedEndDate,
+      selectedNode: this.selectedNode
     };
 
-    // this.http.post<any[]>('http://localhost:8082/api/job-builds-by-time-range-picker', dateData)
-    // .subscribe(
-
     this.http.post<any[]>('http://localhost:8081/app/api/job-builds-by-time-range-picker', dateData)
-    .subscribe(
-      (response) => {
-        console.log('Server response :', response);
-        this.jobBuilds = response;
-        this.builds = response.map(item => ({ jobname: item.jobname,buildnumber: item.buildnumber, duration: item.duration }));
-        this.DisplayTable = true;
-      },
-      (error) => {
-        console.error('Error while submitting the form :', error);
-      }
-    );
+      .subscribe(
+        (response) => {
+          console.log('Server response:', response);
+          this.jobBuilds = response;
+          this.builds = response.map(item => ({ jobname: item.jobname, buildnumber: item.buildnumber, duration: item.duration }));
+          this.DisplayTable = true;
+        },
+        (error) => {
+          console.error('Error while submitting the form:', error);
+        }
+      );
   }
 
   getBarWidth(duration: string) {
